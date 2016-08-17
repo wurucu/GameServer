0920d4241794bbaa33de4fea0bdb4d69c078f40e
coquicox <nicolasroy96@gmail.com>
2016-08-17 03:22:07 +0200

    Added give command

---
+++ GameServer/Logic/Chatbox/Commands/GiveCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ENet;
+using static LeagueSandbox.GameServer.Logic.Chatbox.ChatboxManager;
+
+namespace LeagueSandbox.GameServer.Logic.Chatbox.Commands
+{
+    class GiveCommand : ChatCommand
+    {
+        public GiveCommand(string command, string syntax, ChatboxManager owner) : base(command, syntax, owner){}
+
+        public override void Execute(Peer peer, bool hasReceivedArguments, string arguments = "")
+        {
+            var split = arguments.ToLower().Split(' ');
+            int idItem;
+            if (split.Length < 2)
+            {
+                _owner.SendDebugMsgFormatted(DebugMsgType.SYNTAXERROR);
+                ShowSyntax();
+            }
+            else if (int.TryParse(split[1], out idItem))
+            {
+                var itemTemplate = _owner.GetGame().ItemManager.SafeGetItemType(idItem);
+                var i = _owner.GetGame().GetPeerInfo(peer).GetChampion().Inventory.AddItem(itemTemplate);
+                _owner.GetGame().PacketNotifier.notifyItemBought(_owner.GetGame().GetPeerInfo(peer).GetChampion(), i);
+            }
+        }
+    }
+}
